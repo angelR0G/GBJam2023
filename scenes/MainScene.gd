54c7996e2ab29b22de2b85e2c1ef1d817e737344
angelR0G <angelroblesgonzalez80@gmail.com>
2023-09-18 11:27:27 +0200

    feat: new levels and planet change

--- scenes/MainScene.gd
+++ scenes/MainScene.gd
@@ -1,27 +1,34 @@
 extends Node2D
 
-var planets:Array[Planet] = [Planet1]
+var planets:Array[Planet] = [Planet1, Planet2]
 var currentPlanet:int = 0
 var levelScript = preload("res://scripts/level.gd").new()
 var levelMap:Array[PackedScene]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	newMap()
+	
+func newMap():
 	levelMap = levelScript.generateMap(planets[currentPlanet])
 	renderLevel()
 	for i in levelMap:
 		print(i.instantiate().get_name())
-	
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("ui_left"):
-		levelScript.prev_level()
+		var changePlanet = levelScript.prev_level()
+		changePlanet(changePlanet)
 		renderLevel()
 	if Input.is_action_just_pressed("ui_right"):
 		levelScript.next_level()
 		renderLevel()
+
+func changePlanet(changePlanet:bool):
+	if(changePlanet && currentPlanet < planets.size()-1):
+		currentPlanet += 1
+		newMap()
 
 func renderLevel():
 	#Remove previous level
--- scripts/level.gd
+++ scripts/level.gd
@@ -4,6 +4,7 @@ extends Node
 var map:Array[PackedScene]
 var currentLevel:int 	= 0
 var loadedLevel
+var lastLevel:bool = false
 
 #Selects random levels from the planet level pool
 func generateMap(planet:Planet) -> Array[PackedScene]:
@@ -16,10 +17,19 @@ func generateMap(planet:Planet) -> Array[PackedScene]:
 	return map
 		
 	
-func next_level():
+func next_level() -> bool:
 	if currentLevel < map.size()-1:
 		currentLevel += 1
+	if currentLevel < map.size():
+		lastLevel = true
+	return lastLevel
 		
-func prev_level():
+func prev_level() -> bool:
 	if currentLevel > 0:
 		currentLevel -= 1
+	if lastLevel && currentLevel == 0:
+		#change planet
+		currentLevel = 0
+		return true
+	return false
+		
