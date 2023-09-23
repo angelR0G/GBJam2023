f0bbfc4d5956609faeb9b6657bb04d6a4f3e957e
angelR0G <angelroblesgonzalez80@gmail.com>
2023-09-23 18:16:44 +0200

    fix: add levels

--- scripts/PlanetContainer.gd
+++ scripts/PlanetContainer.gd
@@ -3,7 +3,7 @@ extends Node
 var planetStruct = preload("res://scripts/PlanetStruct.gd")
 
 @onready var planets = 	[	
-							[ planetStruct.new(Planet1, false), planetStruct.new(Planet1, false), planetStruct.new(Planet1, false) ],
-							[ planetStruct.new(Planet1, false), planetStruct.new(Planet1, false), planetStruct.new(Planet1, false) ],
-							[ planetStruct.new(Planet1, false), planetStruct.new(Planet1, false), planetStruct.new(Planet1, false) ]
+							[ planetStruct.new(Planet1, false), planetStruct.new(Planet2, false), planetStruct.new(Planet3, false) ],
+							[ planetStruct.new(Planet4, false), planetStruct.new(Planet5, false), planetStruct.new(Planet6, false) ],
+							[ planetStruct.new(Planet7, false), planetStruct.new(Planet8, false), planetStruct.new(Planet9, false) ]
 						]
